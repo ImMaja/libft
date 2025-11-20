a4285fe235d9070667f26c42e5f433e5e441142e
maja <guill.peiffer@gmail.com>
2025-11-20 15:34:18 +0100

    .

--- srcs/libft/ft_str_has_exactly.c
+++ srcs/libft/ft_str_has_exactly.c
@@ -9,7 +9,5 @@
  * false otherwise
  */
 bool	ft_str_has_exactly(const char *s, const size_t len) {
-	size_t	n = ft_strnlen(s, len + 1);
-
-	return (len == n);
+	return (ft_strnlen(s, len + 1) == len);
 }
